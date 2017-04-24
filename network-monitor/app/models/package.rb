c41fc0014f12bcaafae629e6241c2e62baeee2fd
Piotr Konsek <piotrkonsek@gmail.com>
2017-04-24 21:54:25 +0200

    add db seed, index view for packages

--- network-monitor/app/models/package.rb
+++ network-monitor/app/models/package.rb
@@ -2,7 +2,7 @@ class Package
   include Mongoid::Document
   field :from_ip, type: String
   field :to_ip, type: String
-  field :time, type: Timestamp
+  field :time, type: String
   field :protocol, type: String
   field :is_tcp, type: Mongoid::Boolean
 end
--- network-monitor/db/seeds.rb
+++ network-monitor/db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+(1..100).each do |i|
+  Package.create(
+      from_ip: Faker::Internet.ip_v4_address,
+      to_ip: Faker::Internet.ip_v4_address,
+      time: Faker::Time.between(Time.now - 2.week, Time.now).to_s,
+      protocol: ['ssh', 'http', 'ssh'].sample,
+      is_tcp: Faker::Boolean)
+end
+
+
+# youtube addresses from one address in 4 minutes interval
+fixed_ip_adress = Faker::Internet.ip_v4_address
+start_time = Time.now - 1.week
+
+(1..10).each do |i|
+  Package.create(
+      from_ip: fixed_ip_adress,
+      to_ip: ['208.65.153.238', '208.65.153.251', '208.65.153.253', '208.117.236.69'].sample,
+      time: start_time + 4.minute * i,
+      protocol: 'http',
+      is_tcp: true)
+end
+
