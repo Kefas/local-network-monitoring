2acd4b09b1bf6a04d1c4f653e487c9ed96bb2ba2
alaczyz <alaczyz@gmail.com>
2017-04-27 22:47:24 +0200

    [backend] poprawienie wyłuskiwania - pominięcie rutera, - trzeba sprawidzić czy działa poprawnie

--- backend/src/loopDump.sh
+++ backend/src/loopDump.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
+#calll it with sudo
 
 # check if install
 # check if can be done
 NUM_OF_PACKETS_TO_EXIT=2000
 NUM_OF_TEMP_FILES=100
 while true; do
-     for ((N=0; N<=$NUM_OF_TEMP_FILES; N++)); do
-        sudo tcpdump -v  -c $NUM_OF_PACKETS_TO_EXIT > tmpfile$N.txt
+     for ((N=1; N<=$NUM_OF_TEMP_FILES; N++)); do
+        tcpdump -v  -c $NUM_OF_PACKETS_TO_EXIT > tmpfile$N.txt
         ./parser.sh  tmpfile$N.txt &
 
     done
--- backend/src/parser.sh
+++ backend/src/parser.sh
@@ -8,7 +8,14 @@ fi;
 FILE=$1
 echo "START parse: " $FILE
 
-sed -n '/\(^[0-9][0-9]:[0-9][0-9]:[0-9][0-9].[0-9][0-9][0-9][0-9][0-9]\)/ {N; s/[\r\n]   //g; p}'  $FILE | cut -s --delimiter=' ' --fields=1,14,18,20  |while read P; do   mongo --eval "db.sniff5.insertOne($(./createDoc.sh $P))" &> /dev/null; done
+#standard, no PTR
+sed -n '/\(^[0-9][0-9]:[0-9][0-9]:[0-9][0-9].[0-9][0-9][0-9][0-9][0-9]\)/ {N; s/[\r\n]   //g; p}'  $FILE | grep -v PTR |  cut -s --delimiter=' ' --fields=1,14,18,20  |while read P; do   mongo --eval "db.sniff5.insertOne($(./createDoc.sh $P))" &> /dev/null; done
+
+#PTR?
+sed -n '/\(^[0-9][0-9]:[0-9][0-9]:[0-9][0-9].[0-9][0-9][0-9][0-9][0-9]\)/ {N; s/[\r\n]   //g; p}'  $FILE | grep PTR | grep -v PTR? |  cut -s --delimiter=' ' --fields=1,14,25,20  |while read P; do   mongo --eval "db.sniff5.insertOne($(./createDoc.sh $P))" &> /dev/null; done
+
+#PTR
+sed -n '/\(^[0-9][0-9]:[0-9][0-9]:[0-9][0-9].[0-9][0-9][0-9][0-9][0-9]\)/ {N; s/[\r\n]   //g; p}'  $FILE | grep PTR? | cut -s --delimiter=' ' --fields=1,14,18,23   |while read P; do   mongo --eval "db.sniff5.insertOne($(./createDoc.sh $P))" &> /dev/null; done
 
 
 echo "END parse: " $FILE
