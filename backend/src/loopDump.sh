656abb5bf09145b2c22199d80c211e20d1ff94d7
alaczyz <alaczyz@gmail.com>
2017-05-27 18:53:08 +0200

    [backend] add properites file

--- backend/src/loopDump.sh
+++ backend/src/loopDump.sh
@@ -1,21 +1,15 @@
 #!/usr/bin/env bash
-#calll it with sudo
+#call me with sudo
 
-# check if install
-# check if can be done
-NUM_OF_PACKETS_TO_EXIT=2000
-NUM_OF_TEMP_FILES=100
+source properties.sh
+
+
 while true; do
      for ((N=1; N<=$NUM_OF_TEMP_FILES; N++)); do
-        tcpdump -v  -c $NUM_OF_PACKETS_TO_EXIT > tmpfile$N.txt
+        tcpdump -v  -c $SIZE_OF_SAMPLE > tmpfile$N.txt
         ./parser.sh  tmpfile$N.txt &
 
+        sleep $INTERVAL_IN_SEC
     done
 
 done
-
-
-##cleaning
-#for N in {1..$NUM_OF_TEMP_FILES}; do
-#        rm tmpfile{$N}.txt
-#done
--- backend/src/parser.sh
+++ backend/src/parser.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
+source properties.sh
+
 
 if [ $# != 1 ]; then
-    echo "Not enough args - lack of file to parse";
+    echo "Parsing failed: Not enough args - lack of file to parse";
     exit  1;
 fi;
 
@@ -9,13 +11,13 @@ FILE=$1
 echo "START parse: " $FILE
 
 #standard, no PTR
-sed -n '/\(^[0-9][0-9]:[0-9][0-9]:[0-9][0-9].[0-9][0-9][0-9][0-9][0-9]\)/ {N; s/[\r\n]   //g; p}'  $FILE | grep -v PTR |  cut -s --delimiter=' ' --fields=1,14,18,20  |while read P; do   mongo --eval "db.sniff5.insertOne($(./createDoc.sh $P))" &> /dev/null; done
+sed -n '/\(^[0-9][0-9]:[0-9][0-9]:[0-9][0-9].[0-9][0-9][0-9][0-9][0-9]\)/ {N; s/[\r\n]   //g; p}'  $FILE | grep -v PTR |  cut -s --delimiter=' ' --fields=1,14,18,20  |while read P; do   mongo --eval "${NAME_OF_DB_COLLECTION}.insertOne($(./createDoc.sh $P))" &> /dev/null; done
 
 #PTR
-sed -n '/\(^[0-9][0-9]:[0-9][0-9]:[0-9][0-9].[0-9][0-9][0-9][0-9][0-9]\)/ {N; s/[\r\n]   //g; p}'  $FILE | grep PTR | grep -v PTR? |  cut -s --delimiter=' ' --fields=1,14,25,20  |while read P; do   mongo --eval "db.sniff5.insertOne($(./createDoc.sh $P))" &> /dev/null; done
+sed -n '/\(^[0-9][0-9]:[0-9][0-9]:[0-9][0-9].[0-9][0-9][0-9][0-9][0-9]\)/ {N; s/[\r\n]   //g; p}'  $FILE | grep PTR | grep -v PTR? |  cut -s --delimiter=' ' --fields=1,14,25,20  |while read P; do   mongo --eval "${NAME_OF_DB_COLLECTION}.insertOne($(./createDoc.sh $P))" &> /dev/null; done
 
 #PTR?
-sed -n '/\(^[0-9][0-9]:[0-9][0-9]:[0-9][0-9].[0-9][0-9][0-9][0-9][0-9]\)/ {N; s/[\r\n]   //g; p}'  $FILE | grep PTR? | cut -s --delimiter=' ' --fields=1,14,18,23   |while read P; do   mongo --eval "db.sniff5.insertOne($(./createDoc.sh $P))" &> /dev/null; done
+sed -n '/\(^[0-9][0-9]:[0-9][0-9]:[0-9][0-9].[0-9][0-9][0-9][0-9][0-9]\)/ {N; s/[\r\n]   //g; p}'  $FILE | grep PTR? | cut -s --delimiter=' ' --fields=1,14,18,23   |while read P; do   mongo --eval "${NAME_OF_DB_COLLECTION}.insertOne($(./createDoc.sh $P))" &> /dev/null; done
 
 
 echo "END parse: " $FILE
