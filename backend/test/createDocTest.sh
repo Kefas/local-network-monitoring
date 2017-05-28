baea75677ba2d246a619078dd4cfc4d28f29e07b
alaczyz <alaczyz@gmail.com>
2017-05-28 22:23:02 +0200

    [backend] from_ip -> from

--- backend/test/createDocTest.sh
+++ backend/test/createDocTest.sh
@@ -31,7 +31,7 @@ fi;
 TEST_NAME="should create document"
 $COMMAND A1 A2 A3 A4 > OUTPUT
 
-if [[ "{package: {\"from_ip\": \"A3\", \"to_ip\" : \"A4\", \"time\": \"$(date +%d.%m.%Y) A1\", \"is_tcp\" : \"false\"}}" == $(< OUTPUT) ]]; then
+if [[ "{\"from\": \"A3\", \"to\" : \"A4\", \"time\": \"$(date +%d.%m.%Y) A1\", \"is_tcp\" : \"false\"}" == $(< OUTPUT) ]]; then
             echo -e "PASSED: $TEST_NAME \n"
       else
             echo -e "FAILED:  $TEST_NAME \n"
